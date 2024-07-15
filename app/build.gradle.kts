7b51a29b15c9b5fe0fb5bd93c42c83ebc22bcf4d
KSC <ksc1008@naver.com>
2024-07-15 14:46:42 +0900

    feat: added dependencies for the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,12 @@ dependencies {
     implementation("androidx.activity:activity:1.8.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("io.mockk:mockk-android:1.13.11")
+    testImplementation("io.mockk:mockk-agent:1.13.11")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("org.robolectric:robolectric:4.11.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
 }
