ac75b28ba553b199109b0a312a2eb208fd2d12b3
Kjamm <happyminig@gmail.com>
2024-07-15 22:28:29 +0900

    충남대 Android_김재민 4주차 Step0 (#4)
    * Initial commit
    
    * docs : README.md 업데이터
    
    * refactor : 지난주차 과제 코드 복사
    
    * docs : 기본 설정 업데이트
    
    * docs : README.md 업데이트
    
    * feat : 검색어 입력시 검색 결과 15개 표시
    
    * feat : 카카오 API를 이용하여, 검색 구현
    
    * 충남대 Android_김재민 3주차 과제 Step 0 (#21)
    
    LGTM
    
    * docs : README.md 업데이트
    
    * refactor : 피드백 반영 (ROOM 삭제)
    
    * refactor : 피드백 반영 with(binding) {...}
    
    * refactor : db.close
    
    * docs : kakaoSDK 사용을 위한 업데이트
    
    * refactor : 라이브러리 추가 업데이트
    
    * refactor : 편의성을 위해 검색 기능 MainActivity -> SearchActivity
    
    * refactor : 2주차 과제 피드백 반영 (로직 분리)
    
    * feat : kakaoMapView 띄우기
    
    * rafactor : 문법 오류 수정
    
    * 충남대 Android_김재민 3주차 과제 Step1 (#22)
    
    * docs : README.md 업데이터
    
    * refactor : 지난주차 과제 코드 복사
    
    * docs : 기본 설정 업데이트
    
    * docs : README.md 업데이트
    
    * feat : 검색어 입력시 검색 결과 15개 표시
    
    * feat : 카카오 API를 이용하여, 검색 구현
    
    * refactor : 피드백 반영
    
    * docs : README.md 업데이트
    
    ---------
    
    Co-authored-by: MyStoryG <bookwriterseo@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,17 @@
+<<<<<<< HEAD
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+=======
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+>>>>>>> search/step2
 }
 
 android {
@@ -8,12 +19,26 @@ android {
     compileSdk = 34
 
     defaultConfig {
+<<<<<<< HEAD
+=======
+        ndk {
+            abiFilters.add("arm64-v8a")
+            abiFilters.add("armeabi-v7a")
+            abiFilters.add("x86")
+            abiFilters.add("x86_64")
+        }
+>>>>>>> search/step2
         applicationId = "campus.tech.kakao.map"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
+<<<<<<< HEAD
+=======
+        resValue("string", "kakao_api_key", getApiKey("KAKAO_API_KEY"))
+        buildConfigField("String", "KAKAO_REST_API_KEY", getApiKey("KAKAO_REST_API_KEY"))
+>>>>>>> search/step2
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -36,16 +61,25 @@ android {
 
     buildFeatures {
         viewBinding = true
+<<<<<<< HEAD
+=======
+        dataBinding = true
+>>>>>>> search/step2
     }
 }
 
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
+<<<<<<< HEAD
+=======
+    implementation("androidx.activity:activity-ktx:1.2.3")
+>>>>>>> search/step2
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+<<<<<<< HEAD
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.kakao.maps.open:android:2.9.5")
@@ -61,3 +95,24 @@ dependencies {
     androidTestImplementation("androidx.test:rules:1.5.0")
     androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
 }
+=======
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.room:room-common:2.6.1")
+    implementation("androidx.room:room-runtime:2.3.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    implementation("androidx.activity:activity:1.8.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    implementation("com.google.code.gson:gson:2.8.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
+    implementation("com.kakao.maps.open:android:2.9.5")
+    kapt("androidx.room:room-compiler:2.3.0")
+}
+
+fun getApiKey(key: String): String = gradleLocalProperties(rootDir, providers).getProperty(key)
+>>>>>>> search/step2
