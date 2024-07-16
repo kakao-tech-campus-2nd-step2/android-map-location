022d04118a4d60b073e7cd2ee92ab398967a5199
ddangcong80 <ddangcong80@gmail.com>
2024-07-16 12:37:06 +0900

    feat: 클릭한 장소의 정보가 BottomSheet 뷰에 표시되도록 구현

--- app/src/main/java/campus/tech/kakao/map/view/MapActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/view/MapActivity.kt
@@ -3,7 +3,9 @@ package campus.tech.kakao.map.view
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
 import campus.tech.kakao.map.BuildConfig
 import campus.tech.kakao.map.R
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
@@ -17,20 +19,29 @@ import com.kakao.vectormap.MapView
 class MapActivity : AppCompatActivity() {
     private lateinit var mapView: MapView
     private lateinit var searchFloatingBtn: ExtendedFloatingActionButton
+    private lateinit var clickedPlaceNameView: TextView
+    private lateinit var clickedPlaceAddressView: TextView
+    private lateinit var clickedPlaceView: LinearLayoutCompat
+    private lateinit var KAKAO_APP_KEY: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-        val KAKAO_APP_KEY = BuildConfig.KAKAO_APP_KEY
+        KAKAO_APP_KEY = BuildConfig.KAKAO_APP_KEY
         KakaoMapSdk.init(this, KAKAO_APP_KEY)
 
         initView()
         setListeners()
         initializeMap()
+        handleIntent()
     }
 
     private fun initView() {
         mapView = findViewById(R.id.mapView)
         searchFloatingBtn = findViewById(R.id.searchFloatingBtn)
+        clickedPlaceNameView = findViewById(R.id.clickedPlaceName)
+        clickedPlaceAddressView = findViewById(R.id.clickedPlaceAddress)
+        clickedPlaceView = findViewById(R.id.clickedPlaceView)
     }
 
     private fun setListeners() {
@@ -54,5 +65,19 @@ class MapActivity : AppCompatActivity() {
                 Log.d("KakaoMap", "onMapReady")
             }
         })
+    }
+
+    private fun handleIntent() {
+        val clickedPlaceName = intent.getStringExtra("clickPlaceName")
+        val clickedPlaceAddress = intent.getStringExtra("clickPlaceAddress")
+
+        if (clickedPlaceName != null && clickedPlaceAddress != null) {
+            showClickedPlaceInfo(clickedPlaceName, clickedPlaceAddress)
+        }
+    }
+
+    private fun showClickedPlaceInfo(clickedPlaceName: String?, clickedPlaceAddress: String?) {
+        clickedPlaceNameView.text = clickedPlaceName
+        clickedPlaceAddressView.text = clickedPlaceAddress
     }
 }
