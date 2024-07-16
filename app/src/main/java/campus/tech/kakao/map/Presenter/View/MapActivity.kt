7864df06d1d7eea37fa2bc9d4a4b02a43ab3a2f6
akuby21 <akuby21@gmail.com>
2024-07-17 03:13:37 +0900

    feat: implement show information with bottom sheet
    바텀시트를 이용하여 위치 정보 출력기능 구현

--- app/src/main/java/campus/tech/kakao/map/Presenter/View/MapActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/Presenter/View/MapActivity.kt
@@ -1,10 +1,14 @@
 package campus.tech.kakao.map.Presenter.View
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
+import android.view.View.VISIBLE
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.ViewModelProvider
 import campus.tech.kakao.map.Base.ViewModelFactory
 import campus.tech.kakao.map.Domain.PlaceRepository
@@ -16,6 +20,9 @@ import com.kakao.vectormap.KakaoMapReadyCallback
 import com.kakao.vectormap.LatLng
 import com.kakao.vectormap.MapLifeCycleCallback
 import com.kakao.vectormap.MapView
+import com.kakao.vectormap.label.LabelOptions
+import com.kakao.vectormap.label.LabelStyle
+import com.kakao.vectormap.label.LabelStyles
 
 
 class MapActivity : AppCompatActivity() {
@@ -23,48 +30,27 @@ class MapActivity : AppCompatActivity() {
     private lateinit var viewModel: MapViewModel
     private lateinit var mapView: MapView
     private lateinit var searchText: TextView
-    private lateinit var latLng : LatLng
+    private lateinit var bottomSheet : ConstraintLayout
+    private lateinit var placeName : TextView
+    private lateinit var placeAddress : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+        var latLng : LatLng = LatLng.from(0.0,0.0)
 
         repository = (application as MyApplication).appContainer.repository
         mapView = findViewById<MapView>(R.id.mapView)
         searchText = findViewById<TextView>(R.id.search)
+        bottomSheet = findViewById<ConstraintLayout>(R.id.bottomSheet)
+        placeName = findViewById<TextView>(R.id.placeName)
+        placeAddress = findViewById<TextView>(R.id.placeAddress)
+
         viewModel =
             ViewModelProvider(this, ViewModelFactory(repository))[MapViewModel::class.java]
 
-        intent.extras?.getInt("id")?.let {
-            viewModel.initPlace(it)
-        }
-
-        viewModel.currentPlace.observe(this@MapActivity){
-            latLng = LatLng.from(it?.y ?: 0.0 ,it?.x ?: 0.0)
-        }
-
-        mapView.start(object : MapLifeCycleCallback() {
-            override fun onMapDestroy() {
-            }
-
-            override fun onMapError(error: Exception) {
-            }
-        }, object : KakaoMapReadyCallback() {
-            override fun onMapReady(kakaoMap: KakaoMap) {
-
-            }
-
-            override fun getPosition(): LatLng {
-                return latLng
-            }
-
-        })
-
-
-        searchText.setOnClickListener {
-            val intent = Intent(this, PlaceSearchActivity::class.java)
-            startActivity(intent)
-        }
-
+        initViewModel()
+        settingMap()
+        setSearchListener()
     }
 
     override fun onResume() {
@@ -76,4 +62,70 @@ class MapActivity : AppCompatActivity() {
         super.onPause()
         mapView.pause()
     }
+    private fun settingMap(){
+        var latLng = LatLng.from(0.0,0.0)
+        var name = ""
+
+        viewModel.currentPlace.observe(this@MapActivity){
+           it?.let{
+               latLng = LatLng.from(it.y,it.x)
+               name = it.name
+
+               bottomSheet.visibility = VISIBLE
+               placeName.text = it.name
+               placeAddress.text = it.address
+
+               save(it.id)
+           }
+        }
+
+        mapView.start(object : MapLifeCycleCallback() {
+            override fun onMapDestroy() {
+            }
+
+            override fun onMapError(error: Exception) {
+            }
+        }, object : KakaoMapReadyCallback() {
+
+            override fun onMapReady(kakaoMap: KakaoMap) {
+                makeMark(kakaoMap,latLng,name)
+            }
+
+            override fun getPosition(): LatLng {
+                return latLng
+            }
+
+        })
+    }
+
+    private fun makeMark(kakaoMap: KakaoMap, latLng: LatLng, name : String?) {
+        val styles = kakaoMap.labelManager?.addLabelStyles(
+            LabelStyles.from(
+                LabelStyle.from(R.drawable.location_pin)
+                .setTextStyles(LABEL_TEXT_SIZE,Color.BLACK)
+                .setZoomLevel(LABEL_ZOOM_LEVEL)
+            )
+        )
+        val options = LabelOptions.from(latLng).setStyles(styles).setTexts(name)
+        val layer = kakaoMap.labelManager?.layer
+
+        layer?.addLabel(options)
+    }
+
+    private fun initViewModel(){
+            intent.extras?.getInt("id")?.let {
+                viewModel.initPlace(it)
+
+    private fun setSearchListener(){
+        searchText.setOnClickListener {
+            val intent = Intent(this, PlaceSearchActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    companion object{
+        private const val LABEL_TEXT_SIZE = 24
+        private const val LABEL_ZOOM_LEVEL = 8
+    }
+
 }
