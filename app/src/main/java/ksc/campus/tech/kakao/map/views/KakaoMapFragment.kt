0bc6c4a532e9d3a115a573049f92f83879d8ac59
KSC <ksc1008@naver.com>
2024-07-16 15:34:32 +0900

    feat: added selected location info indicator on the bottom side of KakaoMapFragment

--- app/src/main/java/ksc/campus/tech/kakao/map/views/KakaoMapFragment.kt
+++ app/src/main/java/ksc/campus/tech/kakao/map/views/KakaoMapFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
@@ -14,7 +16,6 @@ import com.kakao.vectormap.MapLifeCycleCallback
 import com.kakao.vectormap.MapView
 import com.kakao.vectormap.camera.CameraPosition
 import com.kakao.vectormap.camera.CameraUpdateFactory
-import com.kakao.vectormap.label.Label
 import com.kakao.vectormap.label.LabelOptions
 import com.kakao.vectormap.label.LabelStyle
 import com.kakao.vectormap.label.LabelStyles
@@ -25,6 +26,9 @@ import ksc.campus.tech.kakao.map.view_models.SearchActivityViewModel
 import java.lang.Exception
 
 class KakaoMapFragment : Fragment() {
+    private lateinit var locationInfoNameView: TextView
+    private lateinit var locationInfoAddressView: TextView
+
     private lateinit var kakaoMapView: MapView
     private var kakaoMap: KakaoMap? = null
     private val viewModel by activityViewModels<SearchActivityViewModel>()
@@ -61,7 +65,17 @@ class KakaoMapFragment : Fragment() {
     private fun initiateViewModelCallbacks(){
         viewModel.selectedLocation.observe(viewLifecycleOwner){
             updateSelectedLocation(it)
+            setTextVisibility(it != null)
+            updateText(it?.name?:"", it?.address?:"")
         }
+    }
+
+    private fun initiateTextViews(parent: View){
+        locationInfoNameView = parent.findViewById(R.id.text_location_name)
+        locationInfoAddressView = parent.findViewById(R.id.text_location_address)
+
+        setTextVisibility(viewModel.selectedLocation.value != null)
+        updateText(viewModel.selectedLocation.value?.name?:"", viewModel.selectedLocation.value?.address?:"")
     }
 
     override fun onCreateView(
@@ -73,6 +87,7 @@ class KakaoMapFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         initiateKakaoMap(view)
+        initiateTextViews(view)
         initiateViewModelCallbacks()
         super.onViewCreated(view, savedInstanceState)
     }
@@ -106,6 +121,16 @@ class KakaoMapFragment : Fragment() {
         } else {
             changedSelectedPosition(LatLng.from(locationInfo.latitude, locationInfo.longitude))
         }
+    }
+
+    private fun setTextVisibility(visible: Boolean){
+        locationInfoAddressView.isVisible = visible
+        locationInfoNameView.isVisible = visible
+    }
+
+    private fun updateText(name:String, address:String){
+        locationInfoNameView.text = name
+        locationInfoAddressView.text = address
     }
 
     private fun changedSelectedPosition(coordinate: LatLng){
