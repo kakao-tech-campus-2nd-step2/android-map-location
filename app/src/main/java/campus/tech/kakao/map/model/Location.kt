681b9455762767fb524816d1d8f848dd1b6e6c35
이지은 <jieun7056@naver.com>
2024-07-18 01:55:36 +0900

    feat: 키워드로 장소 검색하는 api에서 x, y 값 받아오기

--- app/src/main/java/campus/tech/kakao/map/model/Location.kt
+++ app/src/main/java/campus/tech/kakao/map/model/Location.kt
@@ -3,11 +3,13 @@ package campus.tech.kakao.map.model
 data class Location(
     val title: String,
     val address: String,
-    val category: String
+    val category: String,
+    val x: String,
+    val y: String
 ){
     companion object {
         fun LocationDto.toLocation(): Location {
-            return Location(title, address, category)
+            return Location(title, address, category, x, y)
         }
     }
 }
