3b6608914f5dc3117f641d26dca732b805962719
Kjamm <happyminig@gmail.com>
2024-07-17 22:19:12 +0900

    feat : 검색 결과 선택시 해당 항목의 위치, 장소, 주소 표시

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -1,21 +1,33 @@
 package campus.tech.kakao.map
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import campus.tech.kakao.map.R
 import campus.tech.kakao.map.SearchActivity
 import campus.tech.kakao.map.databinding.ActivityMainBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
 import com.kakao.vectormap.KakaoMapSdk
 import com.kakao.vectormap.LatLng
 import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.camera.CameraAnimation
 import com.kakao.vectormap.camera.CameraUpdateFactory
+import com.kakao.vectormap.label.LabelLayer
+import com.kakao.vectormap.label.LabelOptions
+import com.kakao.vectormap.label.LabelStyle
+import com.kakao.vectormap.label.LabelStyles
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var kakaoMap: KakaoMap
+    private lateinit var labelLayer: LabelLayer
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<FrameLayout>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +39,9 @@ class MainActivity : AppCompatActivity() {
         initializeKakaoSdk()
         initializeMapView()
         setSearchBoxClickListener()
+
+        bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet)
+        binding.bottomSheet.visibility = View.GONE
     }
 
     private fun initializeKakaoSdk() {
@@ -37,8 +52,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initializeMapView() {
-
-        val mapLifeCycleCallback = object : MapLifeCycleCallback() {
+        binding.mapView.start(object : MapLifeCycleCallback() {
             override fun onMapDestroy() {
                 Log.d("MainActivity", "onMapDestroy called")
             }
@@ -46,28 +60,74 @@ class MainActivity : AppCompatActivity() {
             override fun onMapError(error: Exception) {
                 Log.e("MainActivity", "onMapError: ${error.message}")
             }
-        }
-
-        val kakaoMapReadyCallback = object : KakaoMapReadyCallback() {
-            override fun onMapReady(kakaoMap: KakaoMap) {
+        }, object : KakaoMapReadyCallback() {
+            override fun onMapReady(map: KakaoMap) {
+                kakaoMap = map
+                labelLayer = kakaoMap.labelManager?.layer!!
                 Log.d("MainActivity", "Map is ready")
             }
 
             override fun getPosition(): LatLng {
-                Log.d("MainActivity", "getPosition called")
                 return LatLng.from(36.37591485731178, 127.34381616478682)
             }
-        }
-
-        binding.mapView.start(mapLifeCycleCallback, kakaoMapReadyCallback)
+        })
     }
 
     private fun setSearchBoxClickListener() {
         binding.searchBar.setOnClickListener {
             Log.d("MainActivity", "Search box clicked")
             val intent = Intent(this, SearchActivity::class.java)
-            startActivity(intent)
+            startActivityForResult(intent, SEARCH_REQUEST_CODE)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SEARCH_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            data?.let {
+                val placeName = it.getStringExtra("place_name")
+                val roadAddressName = it.getStringExtra("road_address_name")
+                val x = it.getDoubleExtra("x", 0.0)
+                val y = it.getDoubleExtra("y", 0.0)
+                addMarker(placeName, roadAddressName, x, y)
+            }
+        }
+    }
+
+    private fun addMarker(placeName: String?, roadAddressName: String?, x: Double, y: Double) {
+        if (placeName != null && roadAddressName != null) {
+            val position = LatLng.from(y, x)
+            val styles = kakaoMap.labelManager?.addLabelStyles(
+                LabelStyles.from(
+                    LabelStyle.from(R.drawable.marker).setZoomLevel(1),
+                    LabelStyle.from(R.drawable.marker).setZoomLevel(1)
+                )
+            )
+
+            val labelOptions = LabelOptions.from(placeName, position).apply {
+                setTexts(placeName)
+                setStyles(styles ?: LabelStyles.from(LabelStyle.from(R.drawable.marker)))
+            }
+
+            labelLayer.addLabel(labelOptions)
+
+            moveCamera(position)
+            updateBottomSheet(placeName, roadAddressName)
+        }
+    }
+
+    private fun moveCamera(position: LatLng) {
+        kakaoMap.moveCamera(
+            CameraUpdateFactory.newCenterPosition(position),
+            CameraAnimation.from(10, false, false)
+        )
+    }
+
+    private fun updateBottomSheet(placeName: String, roadAddressName: String) {
+        binding.placeName.text = placeName
+        binding.placeAddress.text = roadAddressName
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        binding.bottomSheet.visibility = View.VISIBLE
     }
 
     override fun onResume() {
