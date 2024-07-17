3abbc9bb548f4b59825cf9871e891f5718dfd0d7
이지은 <jieun7056@naver.com>
2024-07-18 00:52:39 +0900

    feat: 저장된 검색어의 제목(title)을 전달하는 콜백 함수 구현

--- app/src/main/java/campus/tech/kakao/map/view/search/OnItemSelectedListener.kt
+++ app/src/main/java/campus/tech/kakao/map/view/search/OnItemSelectedListener.kt
@@ -3,6 +3,7 @@ package campus.tech.kakao.map.view.search
 import campus.tech.kakao.map.model.SavedLocation
 
 interface OnItemSelectedListener {
-    fun addSavedLocation(title: String)
-    fun deleteSavedLocation(item: SavedLocation)
+    fun onAddSavedLocation(title: String)
+    fun onDeleteSavedLocation(item: SavedLocation)
+    fun onUpdateLocationRecyclerView(title: String)
 }
