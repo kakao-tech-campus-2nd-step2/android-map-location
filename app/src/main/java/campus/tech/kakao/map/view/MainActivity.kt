be2f3bfc6a4546297cdccf9ca15e8c9752bf8dec
Dong-Han <donghk77@naver.com>
2024-07-18 13:03:32 +0900

    feat : sharedPreferences 방식 기능 구현 & Add ErrorActivity

--- app/src/main/java/campus/tech/kakao/map/view/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/view/MainActivity.kt
@@ -1,13 +1,11 @@
 package campus.tech.kakao.map.view
 
-import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import campus.tech.kakao.map.R
 import campus.tech.kakao.map.databinding.ActivityMainBinding
@@ -20,31 +18,122 @@ import com.kakao.vectormap.MapAuthException
 import com.kakao.vectormap.MapLifeCycleCallback
 import com.kakao.vectormap.MapView
 import com.kakao.vectormap.camera.CameraUpdateFactory
-import com.kakao.vectormap.label.LabelManager
 import com.kakao.vectormap.label.LabelOptions
 import com.kakao.vectormap.label.LabelStyle
 import com.kakao.vectormap.label.LabelStyles
-import com.kakao.vectormap.shape.MapPoints
+import com.kakao.vectormap.label.LabelTextStyle
 
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val EXTRA_PLACE_LONGITUDE = "extra_place_longitude"
+        const val EXTRA_PLACE_LATITUDE = "extra_place_latitude"
+        const val EXTRA_PLACE_NAME = "extra_place_name"
+        const val EXTRA_PLACE_ADDRESSNAME = "extra_place_addressname"
+    }
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var mapView: MapView
     private lateinit var kakaoMap: KakaoMap
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initKakaoMap()
 
+        initKakaoMap()
+        initSearchEditText()
+        initMapView()
+
+    }
+
+    private fun initKakaoMap() {
+        val keyHash = Utility.getKeyHash(this)
+        Log.d("testt", keyHash)
+        val key = getString(R.string.kakao_api_key)
+        KakaoMapSdk.init(this, key)
+    }
+
+    private fun initSearchEditText() {
         binding.searchEditText.isFocusable = false
         binding.searchEditText.setOnClickListener {
             val intent = Intent(this, SearchActivity::class.java)
-            setCameraPositionLauncher.launch(intent)
+            startActivity(intent)
         }
+    }
+
+    private fun initMapView() {
+        mapView = binding.mapView
+        mapView.start(object : MapLifeCycleCallback() {
+
+            override fun onMapDestroy() {
+                Log.d("testt", "onMapDestory")
+            }
+
+            override fun onMapError(error: Exception) {
+                val errorMessage = when ((error as? MapAuthException)?.errorCode) {
+                    401 -> "API 인증에 실패"
+                    499 -> "서버 통신 실패"
+                    else -> "알 수 없는 오류"
+                }
+                Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
+                val intent = Intent(this@MainActivity, ErrorActivity::class.java)
+                intent.putExtra("error", errorMessage)
+                startActivity(intent)
+            }
+        }, object : KakaoMapReadyCallback() {
+            override fun onMapReady(kakaoMap: KakaoMap) {
+                this@MainActivity.kakaoMap = kakaoMap
+                setCameraPosition()
+                setMarker()
+                setBottomSheet()
+
+            }
+        })
+    }
+
+    private fun setCameraPosition() {
+        val sharedPreferences = getSharedPreferences("PlacePreferences", Context.MODE_PRIVATE)
+        val longitude =
+            sharedPreferences.getString(EXTRA_PLACE_LONGITUDE, "127.108621")?.toDouble() ?: 0.0
+        val latitude =
+            sharedPreferences.getString(EXTRA_PLACE_LATITUDE, "37.402005")?.toDouble() ?: 0.0
+        val cameraUpdate = CameraUpdateFactory.newCenterPosition(LatLng.from(latitude, longitude))
+        Log.d("testt", longitude.toString())
+        kakaoMap.moveCamera(cameraUpdate)
+    }
+
+    private fun setMarker() {
+        val sharedPreferences = getSharedPreferences("PlacePreferences", Context.MODE_PRIVATE)
+        val longitude =
+            sharedPreferences.getString(EXTRA_PLACE_LONGITUDE, "127.108621")?.toDouble() ?: 0.0
+        val latitude =
+            sharedPreferences.getString(EXTRA_PLACE_LATITUDE, "37.402005")?.toDouble() ?: 0.0
+        val placeName = sharedPreferences.getString(EXTRA_PLACE_NAME, "Unkwon")
+        var styles = LabelStyles.from(
+            LabelStyle.from(R.drawable.marker_128).setZoomLevel(10)
+                .setTextStyles(LabelTextStyle.from(32, Color.parseColor("#000000")))
+        )
+
+        styles = kakaoMap.labelManager!!.addLabelStyles(styles!!)
+        kakaoMap.labelManager!!.layer!!.addLabel(
+            LabelOptions.from(LatLng.from(latitude, longitude))
+                .setStyles(styles)
+                .setTexts(placeName)
+        )
+    }
+
+    private fun setBottomSheet() {
+        val sharedPreferences = getSharedPreferences("PlacePreferences", Context.MODE_PRIVATE)
+        val placeName = sharedPreferences.getString(EXTRA_PLACE_NAME, "Unknown Place").toString()
+        val addressName =
+            sharedPreferences.getString(EXTRA_PLACE_ADDRESSNAME, "Unknown Address").toString()
+
+        binding.bottomSheetTitle.text = placeName
+        binding.bottomSheetDescription.text = addressName
     }
 
     override fun onResume() {
@@ -58,73 +147,5 @@ class MainActivity : AppCompatActivity() {
         Log.d("testt", "onPause")
         mapView.pause()
     }
-
-    private fun initKakaoMap() {
-        val keyHash = Utility.getKeyHash(this)
-        Log.d("testt", keyHash)
-        val key = getString(R.string.kakao_api_key)
-        KakaoMapSdk.init(this, key)
-
-        mapView = binding.mapView
-        mapView.start(object : MapLifeCycleCallback() {
-            override fun onMapDestroy() {
-                Log.d("testt", "onMapDestory")
-            }
-            override fun onMapError(error: Exception) {
-                Toast.makeText(
-                    this@MainActivity,
-                    when ((error as MapAuthException).errorCode) {
-                        401 -> "API 인증에 실패"
-                        499 -> "서버 통신 실패"
-                        else -> "알 수 없는 오류"
-                    },
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }, object : KakaoMapReadyCallback() {
-            override fun onMapReady(kakaoMap: KakaoMap) {
-                this@MainActivity.kakaoMap = kakaoMap
-            }
-        })
-    }
-
-    private val setCameraPositionLauncher = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) { result: ActivityResult ->
-        setCameraPosition(result)
-    }
-
-    private fun setCameraPosition(result: ActivityResult) {
-        if (result.resultCode == Activity.RESULT_OK) {
-            val data: Intent? = result.data
-            val longitude = data?.getStringExtra(EXTRA_PLACE_LONGITUDE)?.toDouble() ?: 0.0
-            val latitude = data?.getStringExtra(EXTRA_PLACE_LATITUDE)?.toDouble() ?: 0.0
-            val cameraUpdate = CameraUpdateFactory.newCenterPosition(LatLng.from(latitude, longitude))
-            kakaoMap.moveCamera(cameraUpdate)
-            addMarker(latitude, longitude) // 카메라 위치를 업데이트한 후 마커 추가
-        }
-    }
-
-    private fun addMarker(latitude: Double, longitude: Double) {
-        var styles = LabelStyles.from(
-            "marker",
-            LabelStyle.from(R.drawable.marker).setZoomLevel(8)
-                .setTextStyles(32, Color.BLACK, 1, Color.GRAY).setZoomLevel(15)
-                .setTextStyles(32, Color.BLACK, 1, Color.GRAY).setZoomLevel(15)
-        )
-
-        styles = kakaoMap.labelManager!!.addLabelStyles(styles!!)
-
-        kakaoMap.labelManager!!.layer!!.addLabel(
-            LabelOptions.from(LatLng.from(latitude, longitude))
-                .setStyles(styles)
-        )
-    }
-
-    companion object {
-        const val EXTRA_PLACE_LONGITUDE = "extra_place_longitude"
-        const val EXTRA_PLACE_LATITUDE = "extra_place_latitude"
-    }
-
 }
 
