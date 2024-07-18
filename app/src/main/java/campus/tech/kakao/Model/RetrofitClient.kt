c144494cafe08d6d16e10b715b3ae72dbe72092a
SHEEN <normengdie@pusan.ac.kr>
2024-07-19 03:18:19 +0900

    Test : RetrofitClient

--- app/src/main/java/campus/tech/kakao/Model/RetrofitClient.kt
+++ app/src/main/java/campus/tech/kakao/Model/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-private const val BASE_URL = "https://dapi.kakao.com/"
+const val BASE_URL = "https://dapi.kakao.com/"
 
         val instance: KakaoApiService by lazy {
         val retrofit = Retrofit.Builder()
