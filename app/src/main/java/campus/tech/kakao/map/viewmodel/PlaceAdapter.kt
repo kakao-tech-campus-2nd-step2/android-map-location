5d7d6ec91f9e749c98229052b10af87b5ec8a809
Dong-Han <donghk77@naver.com>
2024-07-18 13:03:47 +0900

    feat : sharedPreferences 방식 기능 구현

--- app/src/main/java/campus/tech/kakao/map/viewmodel/PlaceAdapter.kt
+++ app/src/main/java/campus/tech/kakao/map/viewmodel/PlaceAdapter.kt
@@ -1,8 +1,7 @@
 package campus.tech.kakao.map.viewmodel
 
-import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -12,23 +11,31 @@ import campus.tech.kakao.map.model.Document
 import campus.tech.kakao.map.view.MainActivity
 
 class PlaceAdapter(
+    private val context: Context,
     private var items: List<Document>,
     private val onItemClick: (Document) -> Unit
 ) : RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
 
-    class ViewHolder(private val binding: ItemPlaceBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(private val binding: ItemPlaceBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(document: Document, onItemClick: (Document) -> Unit) {
             binding.placeName.text = document.placeName
             binding.placeLocation.text = document.addressName
             binding.root.setOnClickListener {
                 onItemClick(document)
 
-                val intent = Intent()
-                intent.putExtra(MainActivity.EXTRA_PLACE_LONGITUDE, document.x)
-                intent.putExtra(MainActivity.EXTRA_PLACE_LATITUDE, document.y)
-                (binding.root.context as Activity).setResult(Activity.RESULT_OK, intent)
-                (binding.root.context as Activity).finish()
+                
+                val sharedPreferences = context.getSharedPreferences("PlacePreferences", Context.MODE_PRIVATE)
+                val editor = sharedPreferences.edit()
+                editor.putString(MainActivity.EXTRA_PLACE_LONGITUDE, document.x)
+                editor.putString(MainActivity.EXTRA_PLACE_LATITUDE, document.y)
+                editor.putString(MainActivity.EXTRA_PLACE_NAME, document.placeName)
+                editor.putString(MainActivity.EXTRA_PLACE_ADDRESSNAME, document.addressName)
+                editor.apply()
 
+
+                val intent = Intent(context, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                context.startActivity(intent)
             }
         }
     }
