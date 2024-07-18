77eadcd610a72417145f58d99510abd5cfc88473
suminjang <127755029+sumintnals@users.noreply.github.com>
2024-07-18 22:13:39 +0900

    전남대 Android 장수민_4주차 과제(0단계) (#20)
    * feat(Map): 프로젝트 파일 추가
    
    * docs(README): update README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # android-map-location
+
+## 0단계
+위치 검색 코드를 옮겨 온다.
--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -1,11 +1,61 @@
 package campus.tech.kakao.map
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import campus.tech.kakao.map.databinding.ActivityMainBinding
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.MapView
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainViewBinding: ActivityMainBinding
+    private lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainViewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainViewBinding.root)
+
+        mapView = mainViewBinding.mapView
+
+        setUpMapView(mapView)
+
+        mainViewBinding.search.setOnClickListener {
+            val intent = Intent(this@MainActivity,SearchActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.resume()
+    }
+
+    private fun setUpMapView(mapView: MapView) {
+        mapView.start(object : MapLifeCycleCallback() {
+            override fun onMapDestroy() {
+            }
+
+            override fun onMapError(error: Exception) {
+
+            }
+        }, object : KakaoMapReadyCallback() {
+            override fun onMapReady(kakaoMap: KakaoMap) {
+
+            }
+
+            override fun isVisible(): Boolean {
+                return true
+            }
+        })
     }
 }
