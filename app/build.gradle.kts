69139f0844f05bd6e2e0635877342a4df71fa16d
SHEEN <normengdie@pusan.ac.kr>
2024-07-19 03:20:38 +0900

    feat : Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,15 +45,14 @@ android {
 }
 
 dependencies {
-
-        androidTestImplementation("androidx.test:core:1.5.0")
-        androidTestImplementation("androidx.test:core-ktx:1.5.0")
-        androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-        androidTestImplementation("androidx.test.ext:junit:1.1.5")
-        androidTestImplementation("androidx.test.ext:junit-ktx:1.1.5")
-        androidTestImplementation("androidx.test.ext:truth:1.5.0")
-        androidTestImplementation("androidx.test:runner:1.5.2")
-        androidTestUtil("androidx.test:orchestrator:1.4.2")
+    androidTestImplementation("androidx.test:core:1.5.0")
+    androidTestImplementation("androidx.test:core-ktx:1.5.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.5")
+    androidTestImplementation("androidx.test.ext:truth:1.5.0")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestUtil("androidx.test:orchestrator:1.4.2")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.1")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
@@ -68,9 +67,11 @@ dependencies {
     implementation("com.kakao.maps.open:android:2.9.5")
     implementation("com.kakao.sdk:v2-all:2.20.3")
     implementation("androidx.activity:activity:1.8.0")
+    debugImplementation("androidx.fragment:fragment-testing:1.8.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation ("org.robolectric:robolectric:4.9")
 }
 fun getApiKey(key: String): String {
     val properties = Properties()
