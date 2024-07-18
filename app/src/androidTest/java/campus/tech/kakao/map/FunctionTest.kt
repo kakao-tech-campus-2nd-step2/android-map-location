e3913e2fa78dc005293a46d0272501f975dabdca
유지예 <jiyeyu@yujiyeui-MacBookAir.local>
2024-07-18 15:56:22 +0900

    feat: selected word check and click with search

--- app/src/androidTest/java/campus/tech/kakao/map/FunctionTest.kt
+++ app/src/androidTest/java/campus/tech/kakao/map/FunctionTest.kt
@@ -55,17 +55,38 @@ class FunctionTest {
 
                     //2 recyclerview로 검색결과 보이는지 테스트
                     val searchResults = MutableLiveData<List<MapItem>>()
-                    searchResults.postValue(listOf(MapItem("1", "바다 정원", "강원도 고성군", "카페", 127.0, 37.0)))
+                    searchResults.postValue(
+                        listOf(
+                            MapItem(
+                                "1",
+                                "바다 정원",
+                                "강원도 고성군",
+                                "카페",
+                                127.0,
+                                37.0
+                            )
+                        )
+                    )
                     searchActivity.viewModel.setSearchResults(searchResults.value ?: emptyList())
 
-                    val searchResultsRecyclerView: RecyclerView = searchActivity.findViewById(R.id.searchResultsRecyclerView)
+                    val searchResultsRecyclerView: RecyclerView =
+                        searchActivity.findViewById(R.id.searchResultsRecyclerView)
                     searchResultsRecyclerView.adapter?.notifyDataSetChanged()
                     assertEquals(1, searchResultsRecyclerView.adapter?.itemCount)
 
                     //3. 해당 검색결과 중 하나 눌러서 지도 마커표시, bottomsheet 정보 표시
                     searchResultsRecyclerView.findViewHolderForAdapterPosition(0)?.itemView?.performClick()
 
-                    searchActivity.setResultAndFinish(MapItem("0", "바다 정원", "강원도 고성군", "카페", 127.0, 37.0))
+                    searchActivity.setResultAndFinish(
+                        MapItem(
+                            "0",
+                            "바다 정원",
+                            "강원도 고성군",
+                            "카페",
+                            127.0,
+                            37.0
+                        )
+                    )
                     val resultIntent = Intent().apply {
                         putExtra("place_name", "바다 정원")
                         putExtra("road_address_name", "강원도 고성군")
@@ -73,9 +94,15 @@ class FunctionTest {
                         putExtra("y", 37.0)
                     }
 
-                    mainActivity.onActivityResult(MainActivity.SEARCH_REQUEST_CODE, Activity.RESULT_OK, resultIntent)
-                    val bottomSheetTitle: TextView = mainActivity.findViewById(R.id.bottomSheetTitle)
-                    val bottomSheetAddress: TextView = mainActivity.findViewById(R.id.bottomSheetAddress)
+                    mainActivity.onActivityResult(
+                        MainActivity.SEARCH_REQUEST_CODE,
+                        Activity.RESULT_OK,
+                        resultIntent
+                    )
+                    val bottomSheetTitle: TextView =
+                        mainActivity.findViewById(R.id.bottomSheetTitle)
+                    val bottomSheetAddress: TextView =
+                        mainActivity.findViewById(R.id.bottomSheetAddress)
                     assertEquals("바다 정원", bottomSheetTitle.text.toString())
                     assertEquals("강원도 고성군", bottomSheetAddress.text.toString())
 
@@ -88,10 +115,42 @@ class FunctionTest {
                     activity.loadLastMarkerPosition()
 
                     val bottomSheetTitle: TextView = activity.findViewById(R.id.bottomSheetTitle)
-                    val bottomSheetAddress: TextView = activity.findViewById(R.id.bottomSheetAddress)
+                    val bottomSheetAddress: TextView =
+                        activity.findViewById(R.id.bottomSheetAddress)
                     assertEquals("바다 정원", bottomSheetTitle.text.toString())
                     assertEquals("강원도 고성군", bottomSheetAddress.text.toString())
-                    assertEquals(View.VISIBLE, activity.findViewById<FrameLayout>(R.id.bottomSheetLayout).visibility)
+                    assertEquals(
+                        View.VISIBLE,
+                        activity.findViewById<FrameLayout>(R.id.bottomSheetLayout).visibility
+                    )
+
+                    // 5. searchactivity에서 저장된 검색어 남아있는지 확인하고 해당 검색어 누르면 해당 검색어로 재검색되는지 확인하기
+                    val searchEditText: EditText = activity.findViewById(R.id.search_edit_text)
+                    searchEditText.performClick()
+
+                    Executors.newSingleThreadExecutor().execute {
+                        scenarioSearch = ActivityScenario.launch(SearchActivity::class.java)
+                        scenarioSearch.onActivity { searchActivity ->
+
+                            val selectedItemsRecyclerView: RecyclerView =
+                                searchActivity.findViewById(R.id.selectedItemsRecyclerView)
+                            assertEquals(1, selectedItemsRecyclerView.adapter?.itemCount)
+                            val selectedViewHolder =
+                                selectedItemsRecyclerView.findViewHolderForAdapterPosition(0)
+                            assertEquals(
+                                "바다 정원",
+                                selectedViewHolder?.itemView?.findViewById<TextView>(R.id.selectedItemName)?.text.toString()
+                            )
+
+                            selectedViewHolder?.itemView?.performClick()
+                            searchActivity.performSearch("바다 정원")
+
+                            val searchResultsRecyclerView: RecyclerView =
+                                searchActivity.findViewById(R.id.searchResultsRecyclerView)
+                            assertEquals(1, searchResultsRecyclerView.adapter?.itemCount)
+                        }
+                    }
+                }
             }
         }
     }
