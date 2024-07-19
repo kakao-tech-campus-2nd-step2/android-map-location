979a8a8205baf9259bc6b8af4a0d8849490c58b3
Jeong hoon <hoondb@naver.com>
2024-07-19 14:01:25 +0900

    feat: bottomSheet로 선택 방문 지역 표시

--- app/src/main/java/campus/tech/kakao/map/view/MapActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/view/MapActivity.kt
@@ -3,12 +3,14 @@ package campus.tech.kakao.map.view
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import campus.tech.kakao.map.R
 import campus.tech.kakao.map.domain.model.Place
+import campus.tech.kakao.map.ui.BottomSheetFragment
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
 import com.kakao.vectormap.LatLng
@@ -19,69 +21,110 @@ import com.kakao.vectormap.label.LabelOptions
 import com.kakao.vectormap.label.LabelStyle
 import com.kakao.vectormap.label.LabelStyles
 
-
 class MapActivity : AppCompatActivity() {
-    private lateinit var mapView : MapView
+    private lateinit var mapView: MapView
     private lateinit var searchView: SearchView
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
-    private lateinit var resPlace: Place
     private lateinit var kakaoMap: KakaoMap
+    private lateinit var bottomSheetFragment: BottomSheetFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
         mapView = findViewById(R.id.mapView)
-        setResultLauncher()
+        searchView = findViewById(R.id.searchView)
 
-        mapView.start(object : MapLifeCycleCallback(){
-            override fun onMapDestroy() {
-            }
-            override fun onMapError(p0: Exception?) {
-            }
-        },object :KakaoMapReadyCallback(){
+        setResultLauncher()
+        initializeMapView()
+        initializeSearchView()
+    }
+
+    private fun initializeMapView() {
+        mapView.start(object : MapLifeCycleCallback() {
+            override fun onMapDestroy() {}
+            override fun onMapError(p0: Exception?) {}
+        }, object : KakaoMapReadyCallback() {
             override fun onMapReady(map: KakaoMap) {
                 kakaoMap = map
+                initMap()
             }
         })
+    }
 
-        searchView = findViewById(R.id.searchView)
+    private fun initializeSearchView() {
         searchView.setOnClickListener {
             val intent = Intent(this, ViewActivity::class.java)
             resultLauncher.launch(intent)
-
         }
     }
 
-    private fun setResultLauncher(){
-        resultLauncher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result->
-                if(result.resultCode == Activity.RESULT_OK){
-                    val placeData = result.data?.getSerializableExtra("placeData")
-                    if (placeData is Place){
-                        resPlace = placeData
-                        val cameraUpdate = CameraUpdateFactory.newCenterPosition(LatLng.from(placeData.yPos.toDouble(),
-                            placeData.xPos.toDouble()),15)
-                        kakaoMap.moveCamera(cameraUpdate)
-
-
-                        val styles = kakaoMap.labelManager?.
-                        addLabelStyles(LabelStyles.from(LabelStyle.from(campus.tech.kakao.map.R.drawable.icon_location)))
-                        val options: LabelOptions =
-                            LabelOptions.from(LatLng.from(placeData.yPos.toDouble(),
-                                placeData.xPos.toDouble()))
-                                .setStyles(styles)
-
-                        val layer = kakaoMap.labelManager?.layer
-                        layer?.addLabel(options)
-                    }
+    private fun setResultLauncher() {
+        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val placeData = result.data?.getSerializableExtra("placeData") as? Place
+                placeData?.let {
+                    updateMapWithPlaceData(it)
+                    saveLastVisitedPlace(it)
+                    showBottomSheet(it)
                 }
             }
+        }
     }
+
+    private fun updateMapWithPlaceData(place: Place) {
+        val cameraUpdate = CameraUpdateFactory.newCenterPosition(
+            LatLng.from(place.yPos.toDouble(), place.xPos.toDouble()), 15
+        )
+        kakaoMap.moveCamera(cameraUpdate)
+
+        val styles = kakaoMap.labelManager?.addLabelStyles(
+            LabelStyles.from(LabelStyle.from(R.drawable.icon_location))
+        )
+        val options = LabelOptions.from(
+            LatLng.from(place.yPos.toDouble(), place.xPos.toDouble())
+        ).setStyles(styles)
+
+        val layer = kakaoMap.labelManager?.layer
+        layer?.addLabel(options)
+    }
+
+    private fun showBottomSheet(place: Place) {
+        bottomSheetFragment = BottomSheetFragment(place)
+        bottomSheetFragment.show(supportFragmentManager, bottomSheetFragment.tag)
+    }
+
+    private fun initMap() {
+        val sharedPreferences = getSharedPreferences("LastVisitedPlace", MODE_PRIVATE)
+        val placeName = sharedPreferences.getString("placeName", null)
+        val roadAddressName = sharedPreferences.getString("roadAddressName", null)
+        val categoryName = sharedPreferences.getString("categoryName", null)
+        val yPos = sharedPreferences.getString("yPos", null)
+        val xPos = sharedPreferences.getString("xPos", null)
+
+        if (placeName != null && roadAddressName != null && categoryName != null && yPos != null && xPos != null) {
+            val place = Place("", placeName, roadAddressName, categoryName, xPos, yPos)
+            updateMapWithPlaceData(place)
+            showBottomSheet(place)
+        }
+    }
+
+    private fun saveLastVisitedPlace(place: Place) {
+        val sharedPreferences = getSharedPreferences("LastVisitedPlace", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("placeName", place.place)
+        editor.putString("roadAddressName", place.address)
+        editor.putString("categoryName", place.category)
+        editor.putString("yPos", place.yPos)
+        editor.putString("xPos", place.xPos)
+        editor.apply()
+    }
+
     override fun onResume() {
         super.onResume()
         mapView.resume()
-
     }
+
     override fun onPause() {
         super.onPause()
         mapView.pause()
