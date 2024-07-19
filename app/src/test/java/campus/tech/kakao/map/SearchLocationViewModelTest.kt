d162a1519e6b2b971d6e16390f7e4b7f2482ec18
Jeong Suhyeon <jsh00325@naver.com>
2024-07-19 14:57:06 +0900

    test: Test SearchLocationViewModel Init
    - 뷰모델 생성 후 기존의 검색 기록을 잘 가져오는지 확인

--- app/src/test/java/campus/tech/kakao/map/SearchLocationViewModelTest.kt
+++ app/src/test/java/campus/tech/kakao/map/SearchLocationViewModelTest.kt
@@ -1,4 +1,34 @@
 package campus.tech.kakao.map
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import campus.tech.kakao.map.model.SearchLocationRepository
+import campus.tech.kakao.map.viewmodel.SearchLocationViewModel
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
+import org.junit.Assert.*
+import org.junit.Rule
+
 class SearchLocationViewModelTest {
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    private val mockRepository = mockk<SearchLocationRepository>()
+    private lateinit var viewModel: SearchLocationViewModel
+
+    @Test
+    fun testInitViewModel() {
+        // given
+        every { mockRepository.getHistory() } returns listOf("Place1", "Place2")
+
+        // when
+        viewModel = SearchLocationViewModel(mockRepository)
+
+        // then
+        verify { mockRepository.getHistory() }
+        viewModel.history.observeForever(mockk<Observer<List<String>>>(relaxed = true))
+        assertEquals(listOf("Place1", "Place2"), viewModel.history.value)
+    }
 }
