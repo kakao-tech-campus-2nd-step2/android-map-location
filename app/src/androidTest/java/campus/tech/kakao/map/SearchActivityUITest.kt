e3da3c0ab2a897b74f7e510d22432cb9fd345ca1
jmc98 <jmc0820@naver.com>
2024-07-19 17:20:50 +0900

    Test[SearchActivityUITest]: 변경 검색_결과_클릭시 함수 이름

--- app/src/androidTest/java/campus/tech/kakao/map/SearchActivityUITest.kt
+++ app/src/androidTest/java/campus/tech/kakao/map/SearchActivityUITest.kt
@@ -1,25 +1,19 @@
 package campus.tech.kakao.map
 
-import androidx.test.InstrumentationRegistry
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withParent
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import campus.tech.kakao.map.adapter.DocumentAdapter
-import okhttp3.internal.wait
 import org.hamcrest.core.AllOf.allOf
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.lang.Thread.sleep
 
 @RunWith(AndroidJUnit4::class)
 class SearchActivityUITest {
@@ -38,7 +32,7 @@ class SearchActivityUITest {
 	}
 
 	@Test
-	fun 검색_결과_클릭시(){
+	fun 검색_결과_클릭시_검색_결과_나오는지_확인(){
 		val query = "박물관"
 		val search = onView(allOf(withId(R.id.search), withParent(withId(R.id.search_main))))
 		search.perform(replaceText(query))
