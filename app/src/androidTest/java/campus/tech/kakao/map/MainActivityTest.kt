ec6418d777315dc3da3230845c86f8817510e272
LEEMINSEO <real2ms@naver.com>
2024-07-19 17:35:37 +0900

    feat: MainActivityTest

---
+++ app/src/androidTest/java/campus/tech/kakao/map/MainActivityTest.kt
@@ -0,0 +1,35 @@
+package campus.tech.kakao.map
+
+import android.os.SystemClock
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.Visibility
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.withContext
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun displaySearchResults() {
+        onView(withId(R.id.inputSearch)).perform(click())
+        onView(withId(R.id.inputSearch)).perform(typeText("kakao"))
+        SystemClock.sleep(1000)
+        onView(withId(R.id.searchRecyclerView)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+    }
+}
