367e08bed72fee208916637ae1fdcac28c092afb
권성찬 <54511614+ksc1008@users.noreply.github.com>
2024-07-19 20:12:28 +0900

    [경북대 Android_권성찬] 4주차 과제_Step0 (#3)
    * integrated from previous project
    
    * feat: added dependencies for the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,29 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("plugin.serialization") version "1.9.0"
 }
 
+fun getApiKey(key: String): String = gradleLocalProperties(rootDir, providers).getProperty(key)
+
 android {
-    namespace = "campus.tech.kakao.map"
+    namespace = "ksc.campus.tech.kakao.map"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "campus.tech.kakao.map"
+        applicationId = "ksc.campus.tech.kakao.map"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        resValue("string", "KAKAO_API_KEY", getApiKey("KAKAO_API_KEY"))
+        buildConfigField("String", "KAKAO_REST_API_KEY", getApiKey("KAKAO_REST_API_KEY"))
+
     }
 
     buildTypes {
@@ -36,21 +45,25 @@ android {
 
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
 dependencies {
 
+    implementation ("androidx.activity:activity-ktx:1.2.2")
+    implementation ("androidx.fragment:fragment-ktx:1.3.3")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.kakao.maps.open:android:2.9.5")
     implementation("androidx.activity:activity:1.8.0")
-    implementation("androidx.test:core-ktx:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
     testImplementation("junit:junit:4.13.2")
     testImplementation("io.mockk:mockk-android:1.13.11")
     testImplementation("io.mockk:mockk-agent:1.13.11")
