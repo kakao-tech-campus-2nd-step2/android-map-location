9cd5962168705966f8b185a82ceec7307ec4f3a2
이지은 <jieun7056@naver.com>
2024-07-19 20:19:20 +0900

    test: MapAcitivty UI 테스트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,8 +61,10 @@ dependencies {
     implementation("androidx.activity:activity:1.9.0")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:5.8.0")
+    androidTestImplementation("org.mockito:mockito-core:5.8.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.2.0")
 
     implementation("androidx.activity:activity-ktx:1.1.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
