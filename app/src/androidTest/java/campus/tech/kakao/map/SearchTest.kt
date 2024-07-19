832441f3868099c4f0c16d92255f8ce00ba13e0b
todlf <todlfdl@naver.com>
2024-07-19 16:23:34 +0900

    feat: Check deleteSearchWord working

--- app/src/androidTest/java/campus/tech/kakao/map/SearchTest.kt
+++ app/src/androidTest/java/campus/tech/kakao/map/SearchTest.kt
@@ -1,26 +1,20 @@
 package campus.tech.kakao.map
 
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.UiController
-import androidx.test.espresso.ViewAction
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
-import org.hamcrest.BaseMatcher
-import org.hamcrest.CoreMatchers.any
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.hamcrest.Matcher
-import org.junit.runner.Description
-
 
 @RunWith(AndroidJUnit4::class)
 @MediumTest
@@ -45,6 +39,18 @@ class SearchTest {
                     0, click()
                 )
             )
+
+        Thread.sleep(4000)
+
+        pressBack()
+
+        Thread.sleep(2000)
+
+        onView(withId(R.id.deleteSearchWord))
+            .perform(click())
+
+        onView(withId(R.id.searchWord))
+            .check(matches(withText("")))
 
         Thread.sleep(2000)
 
