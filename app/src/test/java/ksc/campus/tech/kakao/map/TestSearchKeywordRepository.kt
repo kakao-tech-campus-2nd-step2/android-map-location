c91bc142d7d347d8e41e86afb6412397c7cc5ab0
KSC <ksc1008@naver.com>
2024-07-20 18:31:29 +0900

    refactor: extracted parameters of initialCamera to separated const values

---
+++ app/src/test/java/ksc/campus/tech/kakao/map/TestSearchKeywordRepository.kt
@@ -0,0 +1,12 @@
+package ksc.campus.tech.kakao.map
+
+import kotlinx.coroutines.runBlocking
+import ksc.campus.tech.kakao.map.models.repositories.MapViewRepository
+import ksc.campus.tech.kakao.map.models.repositories.SearchKeywordRepository
+import ksc.campus.tech.kakao.map.models.repositoriesImpl.MapViewRepositoryImpl
+import ksc.campus.tech.kakao.map.models.repositoriesImpl.SearchKeywordRepositoryImpl
+import org.junit.Before
+import org.junit.Test
+
+class TestSearchKeywordRepository {
+}
