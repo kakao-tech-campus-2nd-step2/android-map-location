c0242a6d7aa48ae785c72b02098796f0109b811d
Jeong Suhyeon <jsh00325@naver.com>
2024-07-21 00:09:12 +0900

    refactor: Remove Intent init & release

--- app/src/androidTest/java/campus/tech/kakao/map/MapErrorActivityTest.kt
+++ app/src/androidTest/java/campus/tech/kakao/map/MapErrorActivityTest.kt
@@ -3,7 +3,6 @@ package campus.tech.kakao.map
 import androidx.lifecycle.Lifecycle
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import campus.tech.kakao.map.view.MapErrorActivity
@@ -17,14 +16,10 @@ class MapErrorActivityTest {
 
     @Test
     fun testClickRefreshButton() {
-        // given
-        Intents.init()
-
         // when
         onView(withId(R.id.refreshBackgroundView)).perform(click())
 
         // then
         assertEquals(Lifecycle.State.DESTROYED, activityRule.scenario.state)
-        Intents.release()
     }
 }
