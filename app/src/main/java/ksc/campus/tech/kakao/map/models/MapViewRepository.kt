6214141ebe7ff997bf16209890d904ee572b21d5
KSC <ksc1008@naver.com>
2024-07-20 20:01:53 +0900

    refactor: extracted data I/O part of the repository to local data source

--- app/src/main/java/ksc/campus/tech/kakao/map/models/MapViewRepository.kt
+++ app/src/main/java/ksc/campus/tech/kakao/map/models/MapViewRepository.kt
@@ -4,13 +4,18 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.kakao.vectormap.camera.CameraPosition
+import kotlinx.serialization.Serializable
+import ksc.campus.tech.kakao.map.models.datasources.MapPreferenceLocalDataSource
 
+
+@Serializable
 data class LocationInfo(val address:String, val name:String, val latitude:Double, val longitude:Double)
 
 class MapViewRepository() {
     private val _selectedLocation: MutableLiveData<LocationInfo?> = MutableLiveData<LocationInfo?>(null)
     private val _cameraPosition: MutableLiveData<CameraPosition> = MutableLiveData(initialCameraPosition)
 
+    private val mapPreferenceDataSource: MapPreferenceLocalDataSource = MapPreferenceLocalDataSource()
     val selectedLocation: LiveData<LocationInfo?>
         get() = _selectedLocation
 
@@ -20,52 +25,20 @@ class MapViewRepository() {
     private fun getZoomCameraPosition(latitude: Double, longitude: Double) = CameraPosition.from(latitude, longitude, 18, 0.0,0.0, -1.0)
 
     private fun saveCurrentPositionToSharedPreference(context:Context, position: CameraPosition){
-        val sharedPreference = context.getSharedPreferences(CURRENT_POSITION_PREFERENCE_NAME, Context.MODE_PRIVATE)
-        val editor = sharedPreference.edit()
-        editor.putInt(CURRENT_POSITION_KEY_ZOOM, position.zoomLevel)
-        editor.putFloat(CURRENT_POSITION_KEY_TILT,position.tiltAngle.toFloat())
-        editor.putFloat(CURRENT_POSITION_KEY_ROTATION,position.rotationAngle.toFloat())
-        editor.putFloat(CURRENT_POSITION_KEY_HEIGHT,position.height.toFloat())
-        editor.putFloat(CURRENT_POSITION_KEY_LATITUDE, position.position.latitude.toFloat())
-        editor.putFloat(CURRENT_POSITION_KEY_LONGITUDE, position.position.longitude.toFloat())
-        editor.apply()
+        mapPreferenceDataSource.saveCameraPosition(context, position)
     }
 
     private fun saveSelectedLocation(context:Context, location: LocationInfo){
-        val sharedPreference = context.getSharedPreferences(SELECTED_LOCATION_PREFERENCE_NAME, Context.MODE_PRIVATE)
-        val editor = sharedPreference.edit()
-        editor.putString(SELECTED_LOCATION_KEY_NAME, location.name)
-        editor.putString(SELECTED_LOCATION_KEY_ADDRESS, location.address)
-        editor.putFloat(SELECTED_LOCATION_KEY_LATITUDE, location.latitude.toFloat())
-        editor.putFloat(SELECTED_LOCATION_KEY_LONGITUDE, location.longitude.toFloat())
-        editor.apply()
+        mapPreferenceDataSource.saveSelectedLocation(context, location)
     }
 
     private fun loadSavedCurrentPosition(context:Context): CameraPosition {
-        val sharedPreference = context.getSharedPreferences(CURRENT_POSITION_PREFERENCE_NAME, Context.MODE_PRIVATE)
-        return CameraPosition.from(
-            sharedPreference.getFloat(CURRENT_POSITION_KEY_LATITUDE, initialCameraPosition.position.latitude.toFloat()).toDouble(),
-            sharedPreference.getFloat(CURRENT_POSITION_KEY_LONGITUDE, initialCameraPosition.position.longitude.toFloat()).toDouble(),
-            sharedPreference.getInt(CURRENT_POSITION_KEY_ZOOM, initialCameraPosition.zoomLevel),
-            sharedPreference.getFloat(CURRENT_POSITION_KEY_TILT, initialCameraPosition.tiltAngle.toFloat()).toDouble(),
-            sharedPreference.getFloat(CURRENT_POSITION_KEY_ROTATION, initialCameraPosition.rotationAngle.toFloat()).toDouble(),
-            sharedPreference.getFloat(CURRENT_POSITION_KEY_HEIGHT, initialCameraPosition.height.toFloat()).toDouble()
-        )
+        val data = mapPreferenceDataSource.getCameraPosition(context)
+        return data?: initialCameraPosition
     }
 
     private fun loadSavedSelectedLocation(context:Context): LocationInfo? {
-        val sharedPreferences =
-            context.getSharedPreferences(SELECTED_LOCATION_PREFERENCE_NAME, Context.MODE_PRIVATE)
-
-        if (!sharedPreferences.contains(SELECTED_LOCATION_KEY_ADDRESS)) {
-            return null
-        }
-        return LocationInfo(
-            sharedPreferences.getString(SELECTED_LOCATION_KEY_ADDRESS, "") ?: "",
-            sharedPreferences.getString(SELECTED_LOCATION_KEY_NAME, "") ?: "",
-            sharedPreferences.getFloat(SELECTED_LOCATION_KEY_LATITUDE, 0.0f).toDouble(),
-            sharedPreferences.getFloat(SELECTED_LOCATION_KEY_LONGITUDE, 0.0f).toDouble()
-        )
+        return mapPreferenceDataSource.getSelectedLocation(context)
     }
 
     fun loadFromSharedPreference(context:Context){
@@ -78,8 +51,8 @@ class MapViewRepository() {
     }
 
     fun updateSelectedLocation(context:Context, locationInfo: LocationInfo){
-        _selectedLocation.postValue(locationInfo)
         saveSelectedLocation(context, locationInfo)
+        _selectedLocation.postValue(locationInfo)
     }
 
     fun updateCameraPositionWithFixedZoom(latitude: Double, longitude: Double){
@@ -87,8 +60,8 @@ class MapViewRepository() {
     }
 
     fun updateCameraPosition(context:Context, position: CameraPosition){
-        _cameraPosition.postValue(position)
         saveCurrentPositionToSharedPreference(context, position)
+        _cameraPosition.postValue(position)
     }
 
     fun clearSelectedLocation(){
@@ -96,20 +69,6 @@ class MapViewRepository() {
     }
 
     companion object {
-        private const val CURRENT_POSITION_PREFERENCE_NAME = "currentPosition"
-        private const val CURRENT_POSITION_KEY_ZOOM = "zoom"
-        private const val CURRENT_POSITION_KEY_TILT = "tilt"
-        private const val CURRENT_POSITION_KEY_ROTATION = "rotation"
-        private const val CURRENT_POSITION_KEY_HEIGHT = "height"
-        private const val CURRENT_POSITION_KEY_LATITUDE = "latitude"
-        private const val CURRENT_POSITION_KEY_LONGITUDE = "longitude"
-
-        private const val SELECTED_LOCATION_PREFERENCE_NAME = "selectedLocation"
-        private const val SELECTED_LOCATION_KEY_NAME = "name"
-        private const val SELECTED_LOCATION_KEY_ADDRESS = "address"
-        private const val SELECTED_LOCATION_KEY_LATITUDE = "latitude"
-        private const val SELECTED_LOCATION_KEY_LONGITUDE = "longitude"
-
         private const val INITIAL_CAMERA_LATITUDE = 35.8905341232321
         private const val INITIAL_CAMERA_LONGITUDE = 128.61213266480294
         private const val INITIAL_CAMERA_ZOOM_LEVEL = 15
