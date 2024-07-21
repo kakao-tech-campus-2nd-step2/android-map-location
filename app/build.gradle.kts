24929ec031ce01c308eced00c89d77977209c10c
sun <50648835+kimseongyu@users.noreply.github.com>
2024-07-22 05:11:22 +0900

    chore: add testOptions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,11 @@ android {
         viewBinding = true
         buildConfig = true
     }
+    testOptions {
+        packaging {
+            resources.excludes.add("META-INF/*")
+        }
+    }
 }
 
 dependencies {
@@ -69,6 +74,7 @@ dependencies {
     implementation("androidx.activity:activity:1.8.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
     implementation("androidx.test.espresso:espresso-contrib:3.6.1")
+    implementation("androidx.test.uiautomator:uiautomator:2.3.0")
     testImplementation("junit:junit:4.13.2")
     testImplementation("io.mockk:mockk:1.13.12")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.1")
