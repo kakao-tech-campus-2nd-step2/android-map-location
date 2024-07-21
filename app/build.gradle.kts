cb5803095065e75fb4b687e4329a1823629db625
yb0x00 <yeongbeen0x00@gmail.com>
2024-07-21 19:50:32 +0900

    test: Add UI test
    [테스트 내용]
    - 검색창을 선택하면 검색 화면으로 이동함
    - 검색어를 입력하면 검색 결과 목록이 나타남
    - 검색 목록에서 선택한 장소의 정보가 지도 화면에 Bottom Sheet로 나타남
    - 오류가 발생하면 오류 화면에 오류 내용이 나타남

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,4 +73,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test:rules:1.5.0")
     androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
+    testImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
+
 }
--- app/src/androidTest/java/campus/tech/kakao/map/MapErrorActivityTest.kt
+++ app/src/androidTest/java/campus/tech/kakao/map/MapErrorActivityTest.kt
@@ -1,25 +1,33 @@
 package campus.tech.kakao.map
 
 import android.content.Intent
-import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import campus.tech.kakao.map.activity.HomeMapActivity
 import campus.tech.kakao.map.activity.MapErrorActivity
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 
 class MapErrorActivityTest {
     @get:Rule
-    val activityRule = ActivityScenarioRule(MapErrorActivity::class.java)
+    val activityRule : ActivityScenarioRule<HomeMapActivity> =
+        ActivityScenarioRule(
+            Intent(
+                ApplicationProvider.getApplicationContext(),
+                MapErrorActivity::class.java
+            ).apply {
+                putExtra("errorMessage","java.lang.Exception:401")
+            }
+        )
 
     @Test
-    fun 오류문구_보여주는지_확인(){
-        onView(withId(R.id.error_notice))
-            .check(matches(withText(R.string.error_text)))
+    fun 오류내용_보여줌() {
+        onView(withId(R.id.error_detail))
+            .check(matches(withText("401")))
     }
 }
