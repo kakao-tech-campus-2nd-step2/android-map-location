829170b2d07abf66038a8d202128625e63de40cc
jmc98 <jmc0820@naver.com>
2024-07-22 14:45:15 +0900

    Comment[]: 삭제 주석

--- app/src/main/java/campus/tech/kakao/map/dto/Document.kt
+++ app/src/main/java/campus/tech/kakao/map/dto/Document.kt
@@ -2,23 +2,15 @@ package campus.tech.kakao.map.dto
 
 import com.google.gson.annotations.SerializedName
 
-//필요 없는 데이터 주석화
 data class Document(
-	//val id: String,
 	@SerializedName("place_name")
 	val placeName: String,
-	//val category_name: String,
-	//val category_group_code: String,
 	@SerializedName("category_group_name")
 	val categoryGroupName: String,
-	//val phone: String,
 	@SerializedName("address_name")
 	val addressName: String,
-	//val road_address_name: String,
 	@SerializedName("x")
 	val longitude : String,
 	@SerializedName("y")
-	val latitude: String,
-	//val place_url: String,
-	//val distance: String,
+	val latitude: String
 )
