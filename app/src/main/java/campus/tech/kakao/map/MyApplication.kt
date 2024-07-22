6049de64818fc5a825c4abe954c5d9d636811ede
jmc98 <jmc0820@naver.com>
2024-07-22 13:54:11 +0900

    Refactor[]: 변경 mapPosition

--- app/src/main/java/campus/tech/kakao/map/MyApplication.kt
+++ app/src/main/java/campus/tech/kakao/map/MyApplication.kt
@@ -1,7 +1,6 @@
 package campus.tech.kakao.map
 
 import android.app.Application
-import campus.tech.kakao.map.dto.MapPositionPreferences
 import com.kakao.vectormap.KakaoMapSdk
 
 class MyApplication : Application() {
