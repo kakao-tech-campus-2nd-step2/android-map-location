a6f241c85dd4e6cbe2bfc2ffd990601a02aabb75
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-07-25 18:16:53 +0900

    feat(Test): 테스트코드 추가

--- app/src/main/java/campus/tech/kakao/map/SearchViewModel.kt
+++ app/src/main/java/campus/tech/kakao/map/SearchViewModel.kt
@@ -1,7 +1,6 @@
 package campus.tech.kakao.map
 
 import android.content.Context
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,8 +10,6 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class SearchViewModel(context: Context) : ViewModel() {
-    private val dbHelper: DBHelper = DBHelper(context)
-    private val db = dbHelper.writableDatabase
     private val preferenceManager = MapApplication.prefs
     var repository = RetrofitRepository()
 
@@ -32,40 +29,6 @@ class SearchViewModel(context: Context) : ViewModel() {
 
     val locationList: LiveData<List<Document>>
         get() = _locationList
-
-    fun insertPlace(place: Place) {
-        dbHelper.insert(db, place)
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        if (db.isOpen) db.close()
-    }
-
-    fun getSearchResult(searchText: String) {
-        if (searchText.isEmpty()) {
-            _placeList.postValue(emptyList())
-        } else {
-            val rDb = dbHelper.readableDatabase
-            val places = mutableListOf<Place>()
-            val query = "SELECT * FROM ${PlaceContract.TABLE_NAME} WHERE ${PlaceContract.TABLE_COLUMN_NAME} LIKE ?"
-            val cursor = rDb.rawQuery(query, arrayOf("%$searchText%"))
-
-            if (cursor != null) {
-                if (cursor.moveToFirst()) {
-                    do {
-                        val name = cursor.getString(cursor.getColumnIndexOrThrow(PlaceContract.TABLE_COLUMN_NAME))
-                        val address = cursor.getString(cursor.getColumnIndexOrThrow(PlaceContract.TABLE_COLUMN_ADDRESS))
-                        val category = cursor.getString(cursor.getColumnIndexOrThrow(PlaceContract.TABLE_COLUMN_CATEGORY))
-                        val place = Place(name, address, category)
-                        places.add(place)
-                    } while (cursor.moveToNext())
-                }
-                cursor.close()
-            }
-            _placeList.postValue(places)
-        }
-    }
 
     fun getSearchHistoryList() {
         _searchHistoryList.value = getSearchHistory()
